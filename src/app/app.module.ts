cc51755c0f004e5e846443c17e54a48f9430d9d3
sjjtu <johntu@live.de>
2021-11-10 08:56:35 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyHeaderComponent } from './my-header/my-header.component';
+import { MyFooterComponent } from './my-footer/my-footer.component';
+import { AboutComponent } from './about/about.component';
+import { CvComponent } from './cv/cv.component';
+import { SamplesComponent } from './samples/samples.component';
+import { StackComponent } from './stack/stack.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHeaderComponent,
+    MyFooterComponent,
+    AboutComponent,
+    CvComponent,
+    SamplesComponent,
+    StackComponent
   ],
   imports: [
     BrowserModule
